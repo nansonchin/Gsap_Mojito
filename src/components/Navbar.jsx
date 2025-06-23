c41eae46b52a9e97aac596e5b07114d783477e15
nansonchin <35142019+nansonchin@users.noreply.github.com>
2025-06-23 15:01:08 +0800

    update navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import {navLinks} from '../../constants/index.js'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+const Navbar = () => {
+    useGSAP(()=>{
+        const navTween = gsap.timeline({
+            scrollTrigger:{
+                trigget:'nav',
+                start:'bottom top',
+                markers:true,
+            }
+        })
+
+        navTween.fromTo('nav',
+            {
+            backgroundColor:'transparent'
+            },
+            {
+                backgroundColor:'0000050',
+                backgroundFilter:'blur(10px)',
+                duration:1,
+                ease:'power1.inOut,'
+            }
+        )
+    })
+    return (
+        <nav>
+            <div>
+                <a href="#home" className='flex items-center gap-2'>
+                    <img src="/images/logo.png" alt="logo"/>
+                    <p>Verlvet Pour</p>
+                </a>
+                <ul>
+                    {navLinks.map((link)=>(
+                        <li>
+                            <a href={`#${link.id}`}>{link.title}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
