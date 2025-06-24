82af16527da91a6de5ba935eef24f6298a5bf227
nansonchin <35142019+nansonchin@users.noreply.github.com>
2025-06-24 11:30:45 +0800

    cocktails detail update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import gsap from 'gsap';
 import {ScrollTrigger,SplitText} from 'gsap/all'
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx';
+import Cocktails from './components/Cocktails.jsx';
 gsap.registerPlugin(ScrollTrigger,SplitText)
 
 const App = () => {
@@ -10,7 +11,7 @@ const App = () => {
     <main>
         <Navbar/>
         <Hero/>
-        <div className='h-[100vh]'></div>
+        <Cocktails/>
     </main>
   )
 }
